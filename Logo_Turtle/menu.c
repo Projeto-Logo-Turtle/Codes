253e9634073e7d3a230425a8db80b75cc3f37e44
Vinícius Ribeiro <vjfr@ecomp.poli.br>
2023-02-20 01:55:14 -0300

    envio dos arquivos menu.c & menu.h

---
+++ Logo_Turtle/menu.c
@@ -0,0 +1,6 @@
+///Arquivo .c para modularizar as funções do menu.
+#include "menu.h"
+
+design(void){
+printf("\n\tLogo Turtle!\nMenu:\n");
+}
---
+++ Logo_Turtle/menu.h
@@ -0,0 +1,7 @@
+///Cabeçalho de pré-processamento das funções do menu.
+#ifndef MENU_H_INCLUDED
+#define MENU_H_INCLUDED
+
+design (void);
+
+#endif // MENU_H_INCLUDED
