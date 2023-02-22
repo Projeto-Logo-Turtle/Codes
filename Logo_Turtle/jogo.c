3ad4e047491659136b6d76b8fa65ac6636fd91be
Vinícius Ribeiro <vjfr@ecomp.poli.br>
2023-02-22 04:03:33 -0300

    Farei esse commit antes que esqueça
    acho que é mais interessante sempre fazer commits, para salvar no tempo pontos do codigo onde se queira voltar e nunca trabalhar diretamente com a branch principal

--- Logo_Turtle/jogo.c
+++ Logo_Turtle/jogo.c
@@ -1,2 +1,21 @@
 
-game(){ system("cls");printf("\n Opção 1 selecionada.\n");}
+game(){ system("cls");printf("\n Lets play!\n");
+
+// acredito que para o jogo, posso fazer uma matriz homogenea ou um struct (heterogeneo) para receber as keywords
+
+typedef struct{
+
+char comando[20];
+int valor;
+
+} entrada; //descobri que posso declarar aqui
+
+
+scanf("%s", entada.comando).
+puts(entrada.comando_0); //- Para teste/debugar
+
+system("pause");
+
+}
+
+/* esqueci o commit de criação das structs8/
