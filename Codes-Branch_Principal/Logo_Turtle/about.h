191278e1c49fb9c239e1bf43f9d75ee0a3708616
Phbulle <106281667+Phbulle@users.noreply.github.com>
2023-03-23 21:30:16 -0300

    Add files via upload

---
+++ Codes-Branch_Principal/Logo_Turtle/about.h
@@ -0,0 +1,8 @@
+#ifndef ABOUT_H_INCLUDED
+#define ABOUT_H_INCLUDED
+
+void gotoxy(int x, int y);
+void teto_cha1();
+void about();
+
+#endif // ABOUT_H_INCLUDED
---
+++ Codes-Branch_Principal/Logo_Turtle/movimento.h
@@ -0,0 +1,6 @@
+#ifndef MOVIMENTO_H_INCLUDED
+#define MOVIMENTO_H_INCLUDED
+///Signature
+int movimenta_tartaruga(char matriz[altura][largura], entr quantidade[20]);
+
+#endif // MOVIMENTO_H_INCLUDED
